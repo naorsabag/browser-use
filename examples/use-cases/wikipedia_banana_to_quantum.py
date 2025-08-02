e962818d3642d2067bb224bb50914549acafdfa4
Naor Sabag <naor.sg@gmail.com>
2025-08-02 19:27:55 +0300

    update example

--- examples/use-cases/wikipedia_banana_to_quantum.py
+++ examples/use-cases/wikipedia_banana_to_quantum.py
@@ -14,10 +14,22 @@ from browser_use.llm import ChatOpenAI
 
 # video https://preview.screen.studio/share/vuq91Ej8
 llm = ChatOpenAI(
-	model='gpt-4.1',
+	model='gpt-4o',
 	temperature=0.0,
+	seed=47,
 )
-task = 'go to https://en.wikipedia.org/wiki/Banana and click on buttons on the wikipedia page to go as fast as possible from banna to Quantum mechanics'
+task = """go to https://en.wikipedia.org/wiki/Banana and click on links on the wikipedia page to go from banna to Quantum mechanics. 
+You can only click on links. 
+You have to click on the links in the order of the path. 
+You must follow this links path: Banana → Banana equivalent dose → Ionizing radiation → Photoelectric effect → Introduction to quantum mechanics -> Quantum mechanics
+First go to https://en.wikipedia.org/wiki/Banana.
+Then scroll down until you find the "Banana equivalent dose" link in the page and click on it.
+Then scroll down until you find the "Ionizing radiation" link in the page and click on it.
+Then scroll down until you find the "Photoelectric effect" link in the page and click on it.
+Then scroll down until you find the "Quantum mechanics" link in the page and click on it.
+You must click on the links in the order of the path.
+You cannot use web or page search.
+"""
 
 browser_session = BrowserSession(
 	browser_profile=BrowserProfile(

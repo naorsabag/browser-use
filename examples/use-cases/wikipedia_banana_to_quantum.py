a22c74057283a82c4a9931801ff4c861e896d050
Naor Sabag <naor.sg@gmail.com>
2025-08-02 20:14:49 +0300

    simplified example

--- examples/use-cases/wikipedia_banana_to_quantum.py
+++ examples/use-cases/wikipedia_banana_to_quantum.py
@@ -9,24 +9,29 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from browser_use import Agent
-from browser_use.browser import BrowserProfile, BrowserSession
 from browser_use.llm import ChatOpenAI
 
 # video https://preview.screen.studio/share/vuq91Ej8
 llm = ChatOpenAI(
-	model='gpt-4.1',
+	model='gpt-4.1-2025-04-14',
 	temperature=0.0,
+	seed=47,
 )
-task = 'go to https://en.wikipedia.org/wiki/Banana and click on buttons on the wikipedia page to go as fast as possible from banna to Quantum mechanics'
+task = """go to https://en.wikipedia.org/wiki/Banana and click on links on the wikipedia page to go from banna to Reddit. 
+You can only click on links. 
+You have to click on the links in the order of the path. 
+You must follow this links path: Banana → Blossom -> Social_media -> Reddit
+First go to https://en.wikipedia.org/wiki/Banana.
+Then scroll down until you find the "Blossom" link in the page and click on it.
+Then scroll down until you find the "Social_media" link in the page and click on it.
+Then scroll down until you find the "Reddit" link in the page and click on it.
+You must click on the links in the order of the path.
+You cannot use web or page search.
+You must save each page content in the file system.
+"""
 
-browser_session = BrowserSession(
-	browser_profile=BrowserProfile(
-		viewport_expansion=-1,
-		highlight_elements=False,
-		user_data_dir='~/.config/browseruse/profiles/default',
-	),
-)
-agent = Agent(task=task, llm=llm, browser_session=browser_session, use_vision=False)
+
+agent = Agent(task=task, llm=llm)
 
 
 async def main():
